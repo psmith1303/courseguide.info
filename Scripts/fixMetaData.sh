4ced052cf2641dfe005d6b78ad15c3390fc0d0f3
psmith1303 <psmith@consulting.co.nz>
2018-06-15 11:46:14 +1200

    Remove un-needed scripts

--- Scripts/fixMetaData.sh
+++
@@ -1,8 +0,0 @@
-aws s3 cp \
-      --exclude "*" \
-      --include "*.css" \
-      --content-type="text/css"  \
-      --metadata-directive="REPLACE" \
-      --recursive \
-       s3://www.courseguide.info/ \
-       s3://www.courseguide.info/
